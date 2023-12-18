2d0833a1e074dd74bdcfbfdd5748f0a5f0912046
Curcudel Teodor <teocurcudel@gmail.com>
2023-12-18 03:36:10 +0200

    Added full data clean and modified data types

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from preprocess import read_csv, handle_missing_values
+from preprocess import read_csv, handle_missing_values, transform_data_types
 
 
 def main():
     file_path = r"C:\Users\Teo\Downloads\HH_Provider_Oct2023.csv"
     data_frame = read_csv(file_path)
     clean_data_frame = handle_missing_values(data_frame)
-
-    # Save the cleaned DataFrame to a new CSV file
-    clean_data_frame.to_csv("clean_data.csv", index=False)
 
     print("Cleaned DataFrame:")
     print(clean_data_frame)
@@ -17,6 +14,13 @@ def main():
 
     print("\n\n\n\n")
     print(clean_data_frame.info())
+
+    transformed_data_frame = transform_data_types(clean_data_frame)
+    print("\n\n\n\n")
+    print(transformed_data_frame.info())
+
+    # save the transformed DataFrame to a new CSV file
+    transformed_data_frame.to_csv("clean_data.csv", index=False)
 
 
 if __name__ == "__main__":
--- preprocess.py
+++ preprocess.py
@@ -28,9 +28,74 @@ def handle_missing_values(data):
     data = data[~mask]
 
     # case 4: data contains leftover symbol on column Type of Ownership
-    data_cleaned = data[data['Type of Ownership'] != '-']
-    data_cleaned = data_cleaned.reset_index(drop=True)
+    data = data[data['Type of Ownership'] != '-']
 
-    return data_cleaned
+    # case 5: further remove - symbol
+    data = data[data['How often patients got better at bathing'] != '-']
+    data = data[data['DTC Numerator'] != '-']
+    data["Telephone Number"] = data["Telephone Number"].apply(lambda x: "0000000000" if x == "-" else x)
+
+    # remove remaining -
+    data.replace("-", pd.NA, inplace=True)
+    data.dropna(how="any", axis=0, inplace=True)
+
+
+    data['Certification Date'] = pd.to_datetime(data['Certification Date'], errors='coerce')
+    # format the date column in the desired format
+    data['Certification Date'] = data['Certification Date'].dt.strftime('%d-%m-%y')
+
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+
+def transform_data_types(data):
+    # 162 -> int
+    # 1,241 -> int
+    # 1.643 -> float
+    # Same As National string -> int8 (we convert it into number 1)
+
+    for col in data.columns[5:]:
+        # check if the column contains a '.'
+        if any('.' in str(value) for value in data[col]):
+            data[col] = pd.to_numeric(data[col], errors='coerce', downcast='float')
+        elif any(',' in str(value) for value in data[col]):
+            # check if all values are integers before converting to int64
+            if all(str(value).replace(',', '').isdigit() for value in data[col]):
+                data[col] = pd.to_numeric(data[col].str.replace(',', ''), errors='coerce', downcast='integer')
+            else:
+                data[col] = pd.to_numeric(data[col].str.replace(',', ''), errors='coerce', downcast='float')
+        elif col == 'ZIP Code':
+            data[col] = pd.to_numeric(data[col], errors='coerce', downcast='integer')
+        else:
+            data[col] = data[col].astype(str)
+
+
+    # encode categorical columns with a mapping dictionary
+    categorization_mapping = {
+        'Worse Than National Rate': 0,
+        'Same As National Rate': 1,
+        'Better Than National Rate': 2
+    }
+
+    categorical_columns = ['DTC Performance Categorization', 'PPR Performance Categorization',
+                               'PPH Performance Categorization']
+    for col in categorical_columns:
+        data[col] = data[col].map(categorization_mapping).astype('Int8')  # Downcast to int8
+
+
+
+    categorization_mapping_yes_no = {
+        'No': 0,
+        'Yes': 1,
+    }
+
+    categorical_columns_yes_no = ['Offers Nursing Care Services', 'Offers Physical Therapy Services',
+                           'Offers Occupational Therapy Services', 'Offers Speech Pathology Services', 'Offers Medical Social Services',
+                           'Offers Home Health Aide Services']
+    for col in categorical_columns_yes_no:
+        data[col] = data[col].map(categorization_mapping_yes_no).astype('Int8')  # Downcast to int8
+
+    return data
 
 
