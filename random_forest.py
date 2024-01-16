7db8fc2def80d8e4f9bf99684ffbc2d30f846014
Curcudel Teodor <teocurcudel@gmail.com>
2024-01-16 16:52:43 +0200

    Improved Random Forest accuracy. Added a SVM Classifier.

--- random_forest.py
+++ random_forest.py
@@ -20,7 +20,7 @@ data['QualityClass'] = pd.cut(data['Quality of patient care star rating'], bins=
 X = data_pca
 y = data['QualityClass']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
 # standardize features
 scaler = StandardScaler()
@@ -28,7 +28,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # create a Random Forest
-random_forest = RandomForestClassifier(random_state=42, n_estimators=100)
+random_forest = RandomForestClassifier(random_state=42, n_estimators=70, bootstrap=True)
 # fit (train) the model on the training data
 random_forest.fit(X_train, y_train)
 
@@ -39,6 +39,9 @@ y_pred = random_forest.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy:.5f}')
 
+
+print(f'Train Accuracy - : {random_forest.score(X_train, y_train):.5f}')
+print(f'Test Accuracy - : {random_forest.score(X_test, y_test):.5f}')
 # param_grid = {
 #     'n_estimators': [100, 200, 300],
 #     'max_depth': [None, 10, 20, 30],
