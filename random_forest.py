72b13a5fa2dcc93cd9feb8d593812c5a8c9c0fb6
Curcudel Teodor <teocurcudel@gmail.com>
2024-01-17 14:18:00 +0200

    Sightly improved accuracy for SVM and removed duplicate code

--- random_forest.py
+++ random_forest.py
@@ -1,26 +1,10 @@
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV
+from utils import get_train_and_test_data as setup
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, mean_squared_error
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv('data/clean_data.csv')
-data_pca = pd.read_csv('data/pca.csv')
+X_train, X_test, y_train, y_test = setup.get_train_and_test_data(test_size=0.2, random_state=101)
 
-# bins and labels -> we make it a classification problem
-bins = [1.0, 1.5, 2.5, 3.5, 4.5, 5.1]
-labels = [1, 2, 3, 4, 5]
-
-# bucket the target variable
-data['QualityClass'] = pd.cut(data['Quality of patient care star rating'], bins=bins, labels=labels, include_lowest=True)
-
-# features (X) and target variable (y)
-# X = data.drop(['Quality of patient care star rating', 'QualityClass'], axis=1)
-X = data_pca
-y = data['QualityClass']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
 # standardize features
 scaler = StandardScaler()
